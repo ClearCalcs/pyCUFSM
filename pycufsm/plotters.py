f9f6ac1d9739b59553eb6ff75ca8365ca27611fe
joelbenjohn <joelben24@gmail.com>
2020-09-30 00:48:40 -0400

    add markdown cells to notebook

--- pycufsm/plotters.py
+++ pycufsm/plotters.py
@@ -329,17 +329,17 @@ def thecurve3(
         if minopt == 1:
             for j in range(len(modedisplay)):
                 for m in range(len(curve[:, 1, 1]) - 2):
-                    load1 = curve[m, j, 1]
-                    load2 = curve[m + 1, j, 1]
-                    load3 = curve[m + 2, j, 1]
+                    load1 = curve[m, modedisplay[j], 1]
+                    load2 = curve[m + 1, modedisplay[j], 1]
+                    load3 = curve[m + 2, modedisplay[j], 1]
                     if load2 < load1 and load2 <= load3:
                         cr = cr + 1
                         mstring = [
-                            "{0:.2f}, {0:.2f}".format(curve[m + 1, j, 0], curve[m + 1, j, 1])
+                            "{0:.2f}, {0:.2f}".format(curve[m + 1, modedisplay[j], 0], curve[m + 1, modedisplay[j], 1])
                         ]
                         ax2.text(
-                            curve[m + 1, j, 0], curve[m + 1, j, 1] - (ymax - ymin)/20,
-                            "{0:.2f}, {0:.2f}".format(curve[m + 1, j, 0], curve[m + 1, j, 1], color = 'r')
+                            curve[m + 1, modedisplay[j], 0], curve[m + 1, modedisplay[j], 1] - (ymax - ymin)/20,
+                            "{0:.2f}, {0:.2f}".format(curve[m + 1, modedisplay[j], 0], curve[m + 1, modedisplay[j], 1], color = 'r')
                         )
     # ax2.text(picpoint[0], picpoint[1],
     #  "{0:.2f}, {0:.2f}".format(curve[m + 1, j, 0], curve[m + 1, j, 1], color = 'r' )
