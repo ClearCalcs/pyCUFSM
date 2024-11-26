61bc20c5a1c9fb2f915366ea729ff1e676b6673e
smith120bh <42363318+smith120bh@users.noreply.github.com>
2024-11-26 12:58:07 +1100

    Don't require typing_extensions for Python 3.12+ TypedDict has been moved to the standard `typing` import

--- pycufsm/types.py
+++ pycufsm/types.py
@@ -1,7 +1,11 @@
 from typing import Any, List, Literal, Optional, Union
 
 import numpy as np
-from typing_extensions import TypedDict
+
+try:
+    from typing import TypedDict  # Python 3.12+
+except ImportError:
+    from typing_extensions import TypedDict  # Python <3.12
 
 __all__ = [
     "ArrayLike",
